da61ebfc686fa158ee5d6f754826a26954fdd9d6
Julia Tate <julia.e.tate@gmail.com>
2017-11-14 10:29:18 -0600

    Pull in reusable components from previous app. Set up LibraryList data json and import into LibraryReducer. Create LibraryList component for app and pull in data from LibraryReducer.

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
+import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 export default class App extends Component {
 
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View />
+        <View>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
       </Provider>
     );
   }
